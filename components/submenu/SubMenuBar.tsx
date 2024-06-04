476dbcc7f228cf27c1ecf779ce23852d26f15405
JeonYooDeok <jyd910215@gmail.com>
2024-06-04 19:53:03 +0900

    feat : 소통공간 링크주소 변경 / 서브메뉴바변경

--- components/submenu/SubMenuBar.tsx
+++ components/submenu/SubMenuBar.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from "next/link"
 
 export default function BoardSubMenuBar({
   option,
@@ -7,27 +7,28 @@ export default function BoardSubMenuBar({
   option: string
   category: string
 }) {
-  const boardSubOptions: { value: string; name: string }[] = [
-    { value: "frees", name: "자유 게시판" },
-    { value: "markets", name: "나눔장터" },
-    { value: "qnas", name: "QnA" }
-  ]
+  const communitySubOptions: { value: string; params: string; name: string }[] =
+    [
+      { value: "frees", params: "?catid=0&page=1", name: "자유 게시판" },
+      { value: "markets", params: "", name: "나눔장터" },
+      { value: "qnas", params: "", name: "QnA" }
+    ]
   const noticeSubOptions = [
-    { value: "notice", name: "공지사항" },
-    { value: "schedule", name: "일정표" }
+    { value: "notice", params: "", name: "공지사항" },
+    { value: "schedule", params: "", name: "일정표" }
   ]
   const complaintOptions = [
-    { value: "", name: "전체민원" },
-    { value: "", name: "나의민원" }
+    { value: "", params: "", name: "전체민원" },
+    { value: "", params: "", name: "나의민원" }
   ]
   const feeOptions = [
-    { value: "myfee", name: "우리집관리비" },
-    { value: "fee", name: "관리비 상세보기" }
+    { value: "myfee", params: "", name: "우리집관리비" },
+    { value: "fee", params: "", name: "관리비 상세보기" }
   ]
 
   const options =
     option == "community"
-      ? boardSubOptions
+      ? communitySubOptions
       : option == "notice"
         ? noticeSubOptions
         : option == "complaint"
@@ -37,22 +38,28 @@ export default function BoardSubMenuBar({
   return (
     // <div className="w-[1200px] h-14 justify-start items-start gap-[178px] inline-flex">
     // <div>
-      <ul className="flex flex-1 justify-between items-center bg-white gap-2.5 hover:cursor-pointer">
-        {options.map(opt => (
-          <li
+    <ul className="flex flex-1 justify-between items-center bg-white gap-2.5 hover:cursor-pointer">
+      {options.map(opt => (
+        <li
+          key={opt.value}
+          className={`inline-flex w-full justify-center items-center text-lg py-3 px-4 ${
+            opt.value == category
+              ? "text-main_color border-b-2 border-main_color font-semibold"
+              : "text-grey_300 font-normal hover:border-b-2 hover:border-gray-200 "
+          }`}>
+          <Link
             key={opt.value}
-            className={`inline-flex w-full justify-center items-center text-lg py-3 px-4 ${
-              opt.value == category
-                ? "text-main_color border-b-2 border-main_color font-semibold"
-                : "text-grey_300 font-normal hover:border-b-2 hover:border-gray-200 "
-            }`}>
-            <Link
-              key={opt.value}
-              href={option === "fee" ? `/${opt.value}`:`/${option}/${opt.value}`}
-              className={`px-6 text-center text-lg whitespace-nowrap`}>{opt.name}</Link>
-          </li>
-        ))}
-      </ul>
+            href={
+              option === "fee"
+                ? `/${opt.value}`
+                : `/${option}/${opt.value}${opt.params}`
+            }
+            className={`px-6 text-center text-lg whitespace-nowrap`}>
+            {opt.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
     // </div>
     // </div>
   )
